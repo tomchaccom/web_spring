b4b98c211f71b6d51aa196de35b5821bc6a0472e
tomchccom <dreamkms2014@gmail.com>
2025-11-24 15:32:19 +0900

    feat: 스프링 시큐리티 기반 로그인 구현

---
+++ src/main/java/com/example/web_spring/Member/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.web_spring.Member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword()) // 이미 BCrypt 인코딩 저장됨
+                .roles(member.getRole().name().replace("ROLE_", ""))
+                .build();
+    }
+}
+
--- src/main/java/com/example/web_spring/SecurityConfig.java
+++ src/main/java/com/example/web_spring/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.web_spring;
 
+import com.example.web_spring.Member.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,8 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomUserDetailsService customUserDetailsService;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -42,7 +47,9 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
-                );
+                )
+                .userDetailsService(customUserDetailsService);
+
 
         return http.build();
     }
