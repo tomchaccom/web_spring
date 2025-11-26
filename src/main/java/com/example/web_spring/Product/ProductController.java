905a481e3333f9e67c6a3f44a271edff59cddd0f
tomchccom <dreamkms2014@gmail.com>
2025-11-26 23:38:35 +0900

    feat: 헤더에서 쿼리 스트링으로 노트북, 스마트폰, 악세서리 구별

--- src/main/java/com/example/web_spring/Product/ProductController.java
+++ src/main/java/com/example/web_spring/Product/ProductController.java
@@ -1,11 +1,11 @@
 package com.example.web_spring.Product;
 
-import com.example.web_spring.global.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +17,17 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public String productList(Model model) {
+    public String productList(
+            Model model,
+            @RequestParam(required = false) String query) {
 
-        List<Product> products = productRepository.findAll();
-
-        model.addAttribute("products", products);
+        if(query == null){
+            List<Product> products = productRepository.findAll();
+            model.addAttribute("products", products);
+        }else{
+            List<Product> products = productRepository.findByCategoryName(query);
+            model.addAttribute("products", products);
+        }
 
         return "main/product"; //
     }
--- src/main/java/com/example/web_spring/Product/ProductRepository.java
+++ src/main/java/com/example/web_spring/Product/ProductRepository.java
@@ -1,6 +1,15 @@
 package com.example.web_spring.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.category.name = :name")
+    List<Product> findByCategoryName(@Param("name") String name);
+
 }
