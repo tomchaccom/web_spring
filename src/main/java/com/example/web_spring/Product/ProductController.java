070f8bb1b971fced9b86043bc4b70b9b1090111f
tomchccom <dreamkms2014@gmail.com>
2025-11-26 22:03:25 +0900

    feat: 상품 목록 저장 및 상품 상세 보기 적용

--- src/main/java/com/example/web_spring/Product/ProductController.java
+++ src/main/java/com/example/web_spring/Product/ProductController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @GetMapping("/products")
     public String productList(Model model) {
@@ -23,5 +25,12 @@ public class ProductController {
 
         return "main/product"; //
     }
+    @GetMapping("/products/{id}")
+    public String getProductDetail(@PathVariable Long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "products/detail";
+    }
+
 
 }
